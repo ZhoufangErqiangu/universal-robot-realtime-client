438be3c351e05291a06cbb474811c173d1e7bebe
AlxeLiu <liuhlightning@qq.com>
2022-08-17 16:40:38 +0800

    data

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import net from "node:net";
+import {
+  RealtimeInterface,
+  RealtimeInterfaceLayout,
+} from "./RealtimeInterface";
+
+export class UniversalRobotInterface {
+  readonly client: net.Socket;
+  protected connected = false;
+  data?: RealtimeInterface;
+  dataCounter = 0;
+
+  constructor(port: number, host: string) {
+    this.client = net.createConnection(port, host, () => {
+      // connect listener
+      this.connected = true;
+      console.log("universal robot connected");
+    });
+    this.client.on("data", (data) => {
+      this.data = RealtimeInterfaceLayout.decode(data);
+      this.dataCounter++;
+    });
+    this.client.on("end", () => {
+      this.connected = false;
+      console.log("universal robot disconnected");
+    });
+  }
+}
